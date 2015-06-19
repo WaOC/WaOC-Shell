0e60239058ac865b0f3afe1d893eb40424741dec
Evandro Zanatta <ezanatta@tray.net.br>
2015-06-19 16:27:24 +0000

    Adicionando codigo

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# WaOC-Shell
-WaOC in Shell
+# WaOC
+Workflow automation Opencode
+
+--------
+
+## Baixando e executando
+
+O download do WaOC pode ser feito executando o comando abaixo:
+``` shell
+wget https://raw.githubusercontent.com/Tray-FrontEnd/WaOC/master/woac.sh
+```
+
+## Funções disponíveis
+
+A seguir, a lista das funções disponíveis
+
+#### Instalação do OpenCode
+``` shell
+sh waoc.sh install
+```
+
+#### Edição de uma loja
+``` shell
+#sh waoc.sh edit 123456 a25e8f6a5c5b6e5f6a5c6cc5 a557a6cb54a654e6f5c5b6a6d6e6 1
+sh waoc.sh edit {{STORE_ID}} {{API_KEY}} {{API_PASSWORD}} {{THEME_ID}}
+```
+
+#### Modo Work
+O modo work, aciona automáticamente o `opencode watch` e também o `gulp`.
+``` shell
+#sh waoc.sh work 123456
+sh waoc.sh work {{STORE_ID}}
+```
+Para finalizar o modo work, ou seja, desligar o `opencode watch` e o `gulp`, deve ser executado a função **stop**
+
+#### Função Stop
+Finaliza o `opencode watch` e também o `gulp`
+``` shell
+sh waoc.sh stop
+```
+
+## Dependências
+
+- Ruby
+- Gem
+- Node.JS
+- NPM
+
+
+## Dica de ferramenta
+
+O WaOC, funciona muito bem na IDE Cloud9(https://c9.io/).
+O Cloud9 é uma IDE online onde é possível editar seu código-fonte e também acessar um terminal para a execução de comandos e instalação de programas.
+
+No Cloud9, todas as dependências já estão instaladas como o Ruby, Gem, Node.js e o NPM.
---
+++ waoc.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+Version () {
+    echo ""
+    echo "WaOC Alpha v1"
+    echo ""
+}
+
+InstallOpencode () {
+    
+    echo "Instalando OpenCode"
+    sleep 1
+    
+    echo "Baixando os arquivos"
+    wget https://github.com/Tray-FrontEnd/WaOC/raw/master/files/opencode_theme-0.0.2.gem
+    wget https://raw.githubusercontent.com/Tray-FrontEnd/WaOC/master/files/gulpfile.js
+    wget https://raw.githubusercontent.com/Tray-FrontEnd/WaOC/master/files/package.json
+    
+    echo "Instalando OpenCode"
+    gem install opencode_theme-0.0.2.gem
+    
+    echo "Instalando as dependências com NPM"
+    npm install
+    
+    echo "✔ Opencode instalado com sucesso"
+}
+
+Edit () {
+
+    if [ "$1" = "" ] || [ "$2" = "" ] || [ "$3" = "" ] ||[ "$4" = "" ]; then
+        echo "✘ Esta faltando alguns parametros, por favor, leia a documentacao"
+    else
+        mkdir $1
+        cd $1
+        opencode configure $2 $3 $4
+        opencode download
+        cd ..
+        echo "✔ Loja pronta para ser editada"
+    fi
+}
+
+Work () {
+    if [ "$1" = "" ]; then
+        echo "✘ Está faltando o ID da loja"
+    else
+        echo "Iniciando Work..."
+        cd $1
+        opencode watch &
+        cd ..
+        gulp &
+        echo "✔ Modo Work ON"
+    fi
+}
+
+Stop () {
+    pkill ruby
+    pkill opencode
+    pkill gulp
+}
+
+#sh waoc.sh install opencode
+if [ "$1" = "install" ]; then
+    InstallOpencode
+
+#sh waoc.sh edit 123456 a2c22a4f4ed6a5c6 a1a29a6e6f7a62c6 7
+elif [ "$1" = "edit" ]; then
+    Edit $2 $3 $4 $5
+
+#sh waoc.sh work
+elif [ "$1" = "work" ]; then
+    Work $2
+
+#sh waoc.sh stop
+elif [ "$1" = "stop" ]; then
+    Stop
+    Stop
+    echo "✔ Todos os processos foram parados"
+
+#sh waoc.sh version
+elif [ "$1" = "version" ]; then
+    Version
+
+#sem parametros ou com parametros incorretos
+else
+    echo "✘ Funcao nao encontrada, leia a documentacao"
+fi
